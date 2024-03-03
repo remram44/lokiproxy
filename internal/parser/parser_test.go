91b8f7547fc7a8d493020e2c54d42a953e6ed57f
Remi Rampin <remi@rampin.org>
2024-03-03 18:49:42 -0500

    More tests

--- internal/parser/parser_test.go
+++ internal/parser/parser_test.go
@@ -1,11 +1,12 @@
 package parser
 
 import (
+	"bytes"
+	"fmt"
 	"testing"
 )
 
-func check(t *testing.T, original string, expected string) {
-	requiredLabels := map[string]interface{}{"namespace=\"yes\"": nil}
+func check(t *testing.T, original string, expected string, requiredLabels map[string]interface{}) {
 	result, err := ProcessQuery(original, requiredLabels)
 	if err != nil {
 		t.Fatalf("parser error\nexpected: %#v\nerror   : %v", expected, err)
@@ -15,27 +16,90 @@ func check(t *testing.T, original string, expected string) {
 	}
 }
 
+func checkM(t *testing.T, original string, expected []string, requiredLabels map[string]interface{}) {
+	result, err := ProcessQuery(original, requiredLabels)
+	if err != nil {
+		t.Fatalf("parser error\nexpected: %#v\nerror   : %v", expected, err)
+	}
+	for _, possible := range expected {
+		if result == possible {
+			return
+		}
+	}
+	var out bytes.Buffer
+	out.WriteString("parser error\n")
+	for _, possible := range expected {
+		fmt.Fprintf(&out, "expected: %#v\n", possible)
+	}
+	fmt.Fprintf(&out, "result  : %#v", result)
+	t.Fatal(out.String())
+}
+
 func TestLogQuery(t *testing.T) {
+	oneLabel := map[string]interface{}{"namespace=\"yes\"": nil}
 	check(
 		t,
 		"{job_name=\"myapp\"} != ip(\"192.168.4.5-192.168.4.20\")",
 		"{job_name=\"myapp\", namespace=\"yes\"} != ip(\"192.168.4.5-192.168.4.20\")",
+		oneLabel,
 	)
 	check(
 		t,
 		"{job_name=\"myapp\", namespace=\"yes\"} != ip(\"192.168.4.5-192.168.4.20\")",
 		"{job_name=\"myapp\", namespace=\"yes\"} != ip(\"192.168.4.5-192.168.4.20\")",
+		oneLabel,
 	)
 	check(
 		t,
 		"{job_name=\"myapp\",namespace = \"yes\" } != ip(\"192.168.4.5-192.168.4.20\")",
 		"{job_name=\"myapp\",namespace = \"yes\" } != ip(\"192.168.4.5-192.168.4.20\")",
+		oneLabel,
 	)
 
+	oneNeg := map[string]interface{}{"namespace!=\"admin\"": nil}
 	check(
 		t,
-		"123{}",
-		"123{namespace=\"yes\"}",
+		"{}",
+		"{namespace!=\"admin\"}",
+		oneNeg,
+	)
+	check(
+		t,
+		"{ namespace!=\"admin\" }",
+		"{ namespace!=\"admin\" }",
+		oneNeg,
+	)
+
+	twoLabels := map[string]interface{}{"job=\"pods\"": nil, "namespace=~\"yes|oui\"": nil}
+	checkM(
+		t,
+		"{}",
+		[]string{
+			"{job=\"pods\", namespace=~\"yes|oui\"}",
+			"{namespace=~\"yes|oui\", job=\"pods\"}",
+		},
+		twoLabels,
+	)
+	checkM(
+		t,
+		"{audit=~\".+\"}",
+		[]string{
+			"{audit=~\".+\", job=\"pods\", namespace=~\"yes|oui\"}",
+			"{audit=~\".+\", namespace=~\"yes|oui\", job=\"pods\"}",
+		},
+		twoLabels,
+	)
+	check(
+		t,
+		"{  job=\"pods\" }",
+		"{  job=\"pods\" , namespace=~\"yes|oui\"}",
+		twoLabels,
+	)
+	check(
+		t,
+		"{ job=\"pods\" , audit!=\"true\" }",
+		"{ job=\"pods\" , audit!=\"true\" , namespace=~\"yes|oui\"}",
+		twoLabels,
 	)
 }
 
